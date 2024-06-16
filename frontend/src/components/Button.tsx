36b7ec5b00a13ac8bc498c465624b68a00bc7a8e
Turbobot <turbobot@vercel.com>
2024-06-16 07:19:19 -0400

    feat(create-turbo): apply official-starter transform

--- frontend/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-export const Button = ({onClick, children}:{onClick: () => void, children: React.ReactNode}) => {
-    return (
-        <button onClick={onClick} className=" bg-green-500
-        hover:bg-green-700 text-white
-       font-bold py-4 px-8 rounded text-2xl">
-            {children}
-        </button>
-    );
-}
